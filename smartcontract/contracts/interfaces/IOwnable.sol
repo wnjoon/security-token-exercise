1ad2523db15ff29275890551d999613122d22245
ssbc <ssbc@ssbcMac.local>
2022-05-09 14:30:07 +0900

    22.05.09-14:30 : init_smartcontract_directory

---
+++ smartcontract/contracts/interfaces/IOwnable.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.0;
+
+interface IOwnable {
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    function owner() external view returns (address);
+    function transferOwnership(address newOwner) external;
+}
